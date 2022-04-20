b086632001006d4d0a495b6ab5babe79f0495dcc
Rafael Borges <rafael.borges@luizalabs.com>
2022-04-20 20:13:33 -0300

    create category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,64 @@
 import { Category } from "./category";
+import { faker } from "@faker-js/faker";
 
 describe("Category tests", () => {
-  it("category constructor", () => {
-    const category = new Category("movie");
-    expect(category).toBeInstanceOf(Category);
-    expect(category.name).toBe("movie");
+  let name: string;
+  let description: string;
+  let is_active: boolean;
+  let created_at: Date;
+
+  beforeEach(() => {
+    name = faker.name.findName();
+    description = faker.lorem.sentence();
+    is_active = faker.datatype.boolean();
+    created_at = faker.date.past();
+  });
+
+  describe("constructor tests", () => {
+    it("should assert all constructor props", () => {
+      // arrange
+      const categoryProperties = { name, description, is_active, created_at };
+
+      // act
+      const category = new Category(categoryProperties);
+
+      // assert
+      expect(category).toBeInstanceOf(Category);
+      expect(category.name).toEqual(name);
+      expect(category.description).toEqual(description);
+      expect(category.is_active).toEqual(is_active);
+      expect(category.created_at).toEqual(created_at);
+    });
+
+    it("should assert all constructor props with undefined values", () => {
+      // arrange
+      const categoryProperties = { name, description, is_active };
+
+      // act
+      const category = new Category(categoryProperties);
+
+      // assert
+      expect(category).toBeInstanceOf(Category);
+      expect(category.name).toEqual(name);
+      expect(category.description).toEqual(description);
+      expect(category.is_active).toEqual(is_active);
+      expect(category.created_at).toBeUndefined();
+    });
+  });
+
+  describe("getters tests", () => {
+    it("should assert all getters", () => {
+      // arrange
+      const categoryProperties = { name, description, is_active, created_at };
+
+      // act
+      const category = new Category(categoryProperties);
+
+      // assert
+      expect(category.name).toEqual(name);
+      expect(category.description).toEqual(description);
+      expect(category.created_at).toEqual(created_at);
+      expect(category.is_active).toEqual(is_active);
+    });
   });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,26 @@
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
 export class Category {
-  constructor(public name: string) {
-    this.name = name;
+  constructor(public readonly props: CategoryProperties) {}
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get description(): string | undefined {
+    return this.props.description;
+  }
+
+  get is_active(): boolean | undefined {
+    return this.props.is_active;
+  }
+
+  get created_at(): Date | undefined {
+    return this.props.created_at;
   }
 }
