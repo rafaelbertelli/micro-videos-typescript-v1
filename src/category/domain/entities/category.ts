07022571e4bbe03186ddcf4fb12c5c6ee1778ee4
Rafael Borges <rafael.borges@luizalabs.com>
2022-04-20 21:13:05 -0300

    create setters

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,4 +1,4 @@
-export type CategoryProperties = {
+type CategoryProperties = {
   name: string;
   description?: string;
   is_active?: boolean;
@@ -6,7 +6,11 @@ export type CategoryProperties = {
 };
 
 export class Category {
-  constructor(public readonly props: CategoryProperties) {}
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.is_active = this.props.is_active;
+    this.created_at = this.props.created_at;
+  }
 
   get name(): string {
     return this.props.name;
@@ -16,11 +20,23 @@ export class Category {
     return this.props.description;
   }
 
+  private set description(value: string | undefined) {
+    this.props.description = value ?? null;
+  }
+
   get is_active(): boolean | undefined {
     return this.props.is_active;
   }
 
+  private set is_active(value: boolean | undefined) {
+    this.props.is_active = value ?? true;
+  }
+
   get created_at(): Date | undefined {
     return this.props.created_at;
+  }
+
+  private set created_at(value: Date | undefined) {
+    this.props.created_at = value ?? new Date();
   }
 }
