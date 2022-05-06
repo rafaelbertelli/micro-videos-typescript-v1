ff2498062810ce94b5ea7bfd5a54b9576ce03413
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-06 00:18:14 +0000

    abstract value object

---
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -0,0 +1,16 @@
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
+import InvalidUuidError from "../../errors/invalid-uuid-error";
+import ValueObject from "./value-object";
+
+export default class UniqueEntityId extends ValueObject {
+  constructor(public readonly id?: string) {
+    super(id || uuidv4());
+    this.validate();
+  }
+
+  private validate() {
+    if (!uuidValidate(this.id)) {
+      throw new InvalidUuidError();
+    }
+  }
+}
