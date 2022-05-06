adac9f1447ab40485a5adc56976cabecbc5a9330
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-06 18:58:10 +0000

    make abstract

--- src/@seedwork/domain/entity/entity.ts
+++ src/@seedwork/domain/entity/entity.ts
@@ -1,6 +1,6 @@
 import UniqueEntityId from "../value-objects/unique-entity-id.vo";
 
-export default class Entity<Props> {
+export default abstract class Entity<Props> {
   public readonly uniqueEntityId?: UniqueEntityId;
 
   constructor(public readonly props: Props, id?: UniqueEntityId) {
