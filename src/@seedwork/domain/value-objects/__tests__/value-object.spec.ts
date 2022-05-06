d824fc7b3ee53b0175775b4bb41a23dee70462e9
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-06 14:08:46 +0000

    handle toString return

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -15,4 +15,33 @@ describe("Value Object abstract class", () => {
     expect(stub1.value).toBe("test");
     expect(stub2.value).toStrictEqual(stub2property);
   });
+
+  it("should return string representation", () => {
+    // arrange
+    const stub1property = "test";
+    const stub2property = { value: "test" };
+    const stub3property = ["test", "test2"];
+    const stub4property = { value: ["test", "test2"] };
+    const stub5property: undefined = undefined;
+    const stub6property: null = null;
+    const stub7property = new Date();
+
+    // act
+    const stub1 = new StubValueObject(stub1property);
+    const stub2 = new StubValueObject(stub2property);
+    const stub3 = new StubValueObject(stub3property);
+    const stub4 = new StubValueObject(stub4property);
+    const stub5 = new StubValueObject(stub5property);
+    const stub6 = new StubValueObject(stub6property);
+    const stub7 = new StubValueObject(stub7property);
+
+    // assert
+    expect(stub1.toString()).toStrictEqual(stub1property.toString());
+    expect(stub2.toString()).toStrictEqual(stub2property.toString());
+    expect(stub3.toString()).toStrictEqual(stub3property.toString());
+    expect(stub4.toString()).toStrictEqual(stub4property.toString());
+    expect(stub5.toString()).toStrictEqual("");
+    expect(stub6.toString()).toStrictEqual("");
+    expect(stub7.toString()).toStrictEqual(stub7property.toString());
+  });
 });
--- src/@seedwork/domain/value-objects/value-object.ts
+++ src/@seedwork/domain/value-objects/value-object.ts
@@ -8,4 +8,24 @@ export default abstract class ValueObject<Value = any> {
   get value(): Value {
     return this._value;
   }
+
+  toString = () => {
+    try {
+      if (this.value === undefined || this.value === null) {
+        return "";
+      }
+
+      if (typeof this.value === "string") {
+        return this.value;
+      }
+
+      if (typeof this.value === "object") {
+        return this.value.toString();
+      }
+
+      return this.value.toString();
+    } catch (error) {
+      return this.value + "";
+    }
+  };
 }
