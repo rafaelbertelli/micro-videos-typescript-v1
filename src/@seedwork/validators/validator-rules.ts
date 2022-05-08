ae932b484c8b5a45798e7a3c2e07a9cbbc2410e4
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-08 22:35:29 +0000

    create new validations check isEmpty value from external function

--- src/@seedwork/validators/validator-rules.ts
+++ src/@seedwork/validators/validator-rules.ts
@@ -1,4 +1,5 @@
 import ValidationError from "../errors/validation-error";
+import { isEmpty } from "./empty-validation";
 
 export default class ValidatorRules {
   private constructor(private value: any, private property: string) {
@@ -11,38 +12,38 @@ export default class ValidatorRules {
   }
 
   required(): ValidatorRules {
-    const isUndefined = this.value === undefined;
-    const isNull = this.value === null;
-    const isEmptyString =
-      typeof this.value === "string" && this.value.trim() === "";
-    const isEmptyList = Array.isArray(this.value) && this.value.length === 0;
-    const isEmptyObject =
-      typeof this.value === "object" && Object.keys(this.value).length === 0;
-
-    if (
-      isUndefined ||
-      isNull ||
-      isEmptyString ||
-      isEmptyList ||
-      isEmptyObject
-    ) {
+    if (isEmpty(this.value)) {
       throw new ValidationError(`${this.property} is required`);
     }
     return this;
   }
 
   string(): ValidatorRules {
-    if (typeof this.value !== "string") {
+    if (isEmpty(this.value) || typeof this.value !== "string") {
       throw new ValidationError(`${this.property} must be a string`);
     }
     return this;
   }
 
   maxLength(max: number): ValidatorRules {
-    if (this.value.length > max) {
+    if (isEmpty(this.value) || this.value.length > max) {
       throw new ValidationError(
         `${this.property} must be less than ${max} characters`
       );
+    }
+    return this;
+  }
+
+  boolean(): ValidatorRules {
+    if (isEmpty(this.value) || typeof this.value !== "boolean") {
+      throw new ValidationError(`${this.property} must be a boolean`);
+    }
+    return this;
+  }
+
+  number(): ValidatorRules {
+    if (isEmpty(this.value) || typeof this.value !== "number") {
+      throw new ValidationError(`${this.property} must be a number`);
     }
     return this;
   }
