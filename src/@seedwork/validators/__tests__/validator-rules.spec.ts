22c2a1333aa05203cdd5760dd700cd893917fb7c
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-08 22:35:39 +0000

    test new validations

--- src/@seedwork/validators/__tests__/validator-rules.spec.ts
+++ src/@seedwork/validators/__tests__/validator-rules.spec.ts
@@ -18,6 +18,13 @@ describe("ValidatorRules", () => {
     it("should have maxLength method", () => {
       expect(ValidatorRules.values("xp", "to").maxLength).toBeDefined();
     });
+    it("should have boolean method", () => {
+      expect(ValidatorRules.values("xp", "to").boolean).toBeDefined();
+    });
+
+    it("should have number method", () => {
+      expect(ValidatorRules.values("xp", "to").number).toBeDefined();
+    });
   });
 
   describe("error instance", () => {
@@ -70,6 +77,66 @@ describe("ValidatorRules", () => {
     it("should not throw error if value is not empty", () => {
       expect(() => {
         ValidatorRules.values("rafael", "to").required();
+      }).not.toThrowError();
+    });
+  });
+
+  describe("string", () => {
+    it("should throw error if value is not a string", () => {
+      expect(() => {
+        ValidatorRules.values(1, "to").string();
+      }).toThrowError();
+    });
+
+    it("should not throw error if value is a string", () => {
+      expect(() => {
+        ValidatorRules.values("rafael", "to").string();
+      }).not.toThrowError();
+    });
+  });
+
+  describe("maxLength", () => {
+    it("should throw error if value is greater than max", () => {
+      expect(() => {
+        ValidatorRules.values("rafael", "to").maxLength(5);
+      }).toThrowError();
+    });
+
+    it("should not throw error if value is less than max", () => {
+      expect(() => {
+        ValidatorRules.values("rafael", "to").maxLength(10);
+      }).not.toThrowError();
+    });
+  });
+
+  describe("boolean", () => {
+    it("should throw error if value is not a boolean", () => {
+      expect(() => {
+        ValidatorRules.values("rafael", "to").boolean();
+      }).toThrowError();
+    });
+
+    it("should not throw error if value is a boolean", () => {
+      expect(() => {
+        ValidatorRules.values(true, "to").boolean();
+      }).not.toThrowError();
+
+      expect(() => {
+        ValidatorRules.values(false, "to").boolean();
+      }).not.toThrowError();
+    });
+  });
+
+  describe("number", () => {
+    it("should throw error if value is not a number", () => {
+      expect(() => {
+        ValidatorRules.values("rafael", "to").number();
+      }).toThrowError();
+    });
+
+    it("should not throw error if value is a number", () => {
+      expect(() => {
+        ValidatorRules.values(1, "to").number();
       }).not.toThrowError();
     });
   });
