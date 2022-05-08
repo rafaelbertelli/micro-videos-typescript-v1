03fccc485b8f342f6b83b4628e64fc60adf84ca0
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-08 22:34:22 +0000

    test empty cases

---
+++ src/@seedwork/validators/__tests__/empty-validation.spec.ts
@@ -0,0 +1,39 @@
+import { isEmpty } from "../empty-validation";
+
+describe("empty validation", () => {
+  it("should test boolean return of empty validation", () => {
+    const is = {
+      EMPTY: true,
+      NOT_EMPTY: false,
+    };
+
+    const arrange = [
+      { value: undefined, expected: is.EMPTY },
+      { value: null, expected: is.EMPTY },
+      { value: "", expected: is.EMPTY },
+      { value: "   ", expected: is.EMPTY },
+      { value: "uatemsomdiu", expected: is.NOT_EMPTY },
+      { value: "uatemsomdiu   ", expected: is.NOT_EMPTY },
+      { value: "   uatemsomdiu", expected: is.NOT_EMPTY },
+      { value: [], expected: is.EMPTY },
+      { value: [1, 2, 3], expected: is.NOT_EMPTY },
+      { value: {}, expected: is.EMPTY },
+      { value: { a: 1, b: 2 }, expected: is.NOT_EMPTY },
+      { value: true, expected: is.NOT_EMPTY },
+      { value: false, expected: is.NOT_EMPTY },
+      { value: 1, expected: is.NOT_EMPTY },
+      { value: 0, expected: is.NOT_EMPTY },
+      { value: -1, expected: is.NOT_EMPTY },
+      { value: -0, expected: is.NOT_EMPTY },
+      { value: -1.1, expected: is.NOT_EMPTY },
+      { value: -0.1, expected: is.NOT_EMPTY },
+      { value: 1.1, expected: is.NOT_EMPTY },
+      { value: 0.1, expected: is.NOT_EMPTY },
+      { value: new Date(), expected: is.NOT_EMPTY },
+    ];
+
+    arrange.forEach(({ value, expected }) => {
+      expect(isEmpty(value)).toBe(expected);
+    });
+  });
+});
