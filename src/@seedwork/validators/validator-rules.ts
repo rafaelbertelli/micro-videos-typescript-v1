0bef99eb173bb402ce93acded299dc6af999a3e3
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-08 23:07:01 +0000

    remove emptyness check

--- src/@seedwork/validators/validator-rules.ts
+++ src/@seedwork/validators/validator-rules.ts
@@ -19,14 +19,14 @@ export default class ValidatorRules {
   }
 
   string(): ValidatorRules {
-    if (isEmpty(this.value) || typeof this.value !== "string") {
+    if (typeof this.value !== "string") {
       throw new ValidationError(`${this.property} must be a string`);
     }
     return this;
   }
 
   maxLength(max: number): ValidatorRules {
-    if (isEmpty(this.value) || this.value.length > max) {
+    if (this.value.length > max) {
       throw new ValidationError(
         `${this.property} must be less than ${max} characters`
       );
@@ -35,14 +35,14 @@ export default class ValidatorRules {
   }
 
   boolean(): ValidatorRules {
-    if (isEmpty(this.value) || typeof this.value !== "boolean") {
+    if (typeof this.value !== "boolean") {
       throw new ValidationError(`${this.property} must be a boolean`);
     }
     return this;
   }
 
   number(): ValidatorRules {
-    if (isEmpty(this.value) || typeof this.value !== "number") {
+    if (typeof this.value !== "number") {
       throw new ValidationError(`${this.property} must be a number`);
     }
     return this;
