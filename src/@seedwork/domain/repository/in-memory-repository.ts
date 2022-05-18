8b9edc1613143fd4a48d8bc67a1279bc9341c2f9
Rafael Borges <rafaelbertelli89@gmail.com>
2022-05-18 16:53:56 -0300

    clean up code simplifying methods

--- src/@seedwork/domain/repository/in-memory-repository.ts
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -1,3 +1,4 @@
+import NotFoundError from "../../errors/not-found-error";
 import Entity from "../entity/entity";
 import UniqueEntityId from "../value-objects/unique-entity-id.vo";
 import { RepositoryInterface } from "./repository-contracts";
@@ -12,14 +13,8 @@ export default abstract class InMemoryRepository<E extends Entity>
   }
 
   async findById(id: string | UniqueEntityId): Promise<E> {
-    const _id = id.toString();
-    const result = this.items.find((item) => item.id === _id);
-
-    if (!result) {
-      throw new Error(`Entity with id ${_id} not found`);
-    }
-
-    return result;
+    const index = await this.elementIndex(id.toString());
+    return this.items[index];
   }
 
   async findAll(): Promise<E[]> {
@@ -27,21 +22,20 @@ export default abstract class InMemoryRepository<E extends Entity>
   }
 
   async update(entity: E): Promise<void> {
-    const index = this.elementIndex(entity.id);
+    const index = await this.elementIndex(entity.id);
     this.items[index] = entity;
   }
 
   async delete(id: string | UniqueEntityId): Promise<void> {
-    const index = this.elementIndex(id);
+    const index = await this.elementIndex(id);
     this.items.splice(index, 1);
   }
 
-  elementIndex(id: string | UniqueEntityId): number {
-    const _id = id.toString();
-    const index = this.items.findIndex((item) => item.id === _id);
+  protected async elementIndex(id: string | UniqueEntityId): Promise<number> {
+    const index = this.items.findIndex((item) => item.id === id.toString());
 
     if (index === -1) {
-      throw new Error(`Entity with id ${_id} not found`);
+      throw new NotFoundError(`Entity with id ${id} not found`);
     }
 
     return index;
