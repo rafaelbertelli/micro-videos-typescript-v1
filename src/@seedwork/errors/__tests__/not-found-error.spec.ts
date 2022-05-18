b744d500b1b5518c3ae71ac7eb2c23f37d33689b
Rafael Borges <rafaelbertelli89@gmail.com>
2022-05-18 16:52:08 -0300

    create new error type

---
+++ src/@seedwork/errors/__tests__/not-found-error.spec.ts
@@ -0,0 +1,11 @@
+import NotFoundError from "../not-found-error";
+
+describe("NotFoundError", () => {
+  it("should assert error", () => {
+    const error = new NotFoundError("message");
+    expect(error).toBeInstanceOf(Error);
+    expect(error).toBeInstanceOf(NotFoundError);
+    expect(error.name).toBe("NotFoundError");
+    expect(error.message).toBe("message");
+  });
+});
