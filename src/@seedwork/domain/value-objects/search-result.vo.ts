96a352ebc62d29278625e8157ad6c9f7d55c7f6f
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-21 23:06:37 -0300

    create value object

--- src/@seedwork/domain/value-objects/search-result.vo.ts
+++ src/@seedwork/domain/value-objects/search-result.vo.ts
@@ -1,15 +1,14 @@
 import Entity from "../entity/entity";
 import ValueObject from "./value-object";
 
-type SearchProps<E extends Entity, Filter = string> = {
+type SearchProps<E extends Entity, Filter> = {
   items: E[];
   total: number;
   current_page: number;
   per_page: number;
-  last_page: number;
   sort: string | null;
   sort_dir: string | null;
-  filter: Filter;
+  filter: Filter | null;
 };
 
 export default class SearchResult<
@@ -25,7 +24,29 @@ export default class SearchResult<
   readonly sort_dir: string | null;
   readonly filter: Filter;
 
-  constructor(props: SearchProps = {}) {
+  constructor(props: SearchProps<E, Filter>) {
     super(props);
+
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = Math.ceil(this.total / this.per_page);
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      last_page: this.last_page,
+      sort: this.sort,
+      sort_dir: this.sort_dir,
+      filter: this.filter,
+    };
   }
 }
