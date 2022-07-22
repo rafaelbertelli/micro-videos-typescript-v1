62028036430124ac69bd731e87bfff9dca99cee8
Rafael Borges <rafael.borges@luizalabs.com>
2022-07-22 20:46:52 -0300

    use CategoryOutputMapper

--- src/category/application/use-cases/get-category.use-case.ts
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -1,20 +1,14 @@
 import IUseCase from "../../../@seedwork/application/use-case.interface";
 import CategoryRepository from "../../domain/repository/category.repository";
 import { CategoryOutput } from "../dto/category-output.dto";
+import CategoryOutputMapper from "../mapper/category-output.mapper";
 
 export default class GetCategoryUseCase implements IUseCase<Input, Output> {
   constructor(private repository: CategoryRepository.Repository) {}
 
   async execute(input: Input): Promise<Output> {
     const entity = await this.repository.findById(input.id);
-
-    return {
-      id: entity.id,
-      name: entity.name,
-      description: entity.description,
-      is_active: entity.is_active,
-      created_at: entity.created_at,
-    };
+    return CategoryOutputMapper.toCategoryOutput(entity);
   }
 }
 
