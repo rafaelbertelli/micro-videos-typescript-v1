0ef4e90494395d1c95de91de12adcd4a32b13d57
Rafael Borges <rafael.borges@luizalabs.com>
2022-07-23 15:38:30 -0300

    rename method

--- src/@seedwork/application/mapper/pagination-output.mapper.ts
+++ src/@seedwork/application/mapper/pagination-output.mapper.ts
@@ -1,7 +1,7 @@
 import { PaginationOutputDto } from "../dto/pagination-output.dto";
 
 export default class PaginationOutputMapper {
-  static toPaginationOutput(result: PaginationOutputDto): PaginationOutputDto {
+  static toOutput(result: PaginationOutputDto): PaginationOutputDto {
     return {
       total: result.total,
       current_page: result.current_page,
