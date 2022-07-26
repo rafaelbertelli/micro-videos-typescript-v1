4b39917f52c880b377308fc47763e3bbf225c240
Rafael Borges <rafael.borges@luizalabs.com>
2022-07-26 19:56:02 -0300

    test update method

---
+++ src/category/application/use-cases/__tests__/update-category.use-case.spec.ts
@@ -0,0 +1,67 @@
+import NotFoundError from "../../../../@seedwork/errors/not-found-error";
+import Category from "../../../domain/entities/category";
+import CategoryInMemoryRepository from "../../../infra/category-in-memory.repository";
+import UpdateCategoryUseCase from "../update-category.use-case";
+
+describe("UpdateCategoryUseCase", () => {
+  let usecase: UpdateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    usecase = new UpdateCategoryUseCase(repository);
+  });
+
+  it("should throw error when entity is not found", async () => {
+    await usecase.execute({ id: "1", name: "name" }).catch((error) => {
+      expect(error).toBeInstanceOf(NotFoundError);
+    });
+
+    await expect(() =>
+      usecase.execute({ id: "1", name: "name" })
+    ).rejects.toThrow(new NotFoundError("Entity with id 1 not found"));
+  });
+
+  it("should update a category changing name, description and is_active", async () => {
+    const spyUpdate = jest.spyOn(repository, "update");
+
+    const category = new Category({
+      name: "Category 1",
+      description: "Category 1 description",
+    });
+
+    repository.items = [category];
+
+    const updatedCategory = await usecase.execute({
+      id: category.id,
+      name: "Category 1 updated",
+      description: "Category 1 description updated",
+      is_active: true,
+    });
+
+    expect(spyUpdate).toHaveBeenCalledTimes(1);
+    expect(updatedCategory.name).toBe("Category 1 updated");
+    expect(updatedCategory.description).toBe("Category 1 description updated");
+    expect(updatedCategory.is_active).toBeTruthy();
+  });
+
+  it("should update a category setting is_active to false", async () => {
+    const category = new Category({
+      name: "Category 1",
+      description: "Category 1 description",
+    });
+
+    repository.items = [category];
+
+    const updatedCategory = await usecase.execute({
+      id: category.id,
+      name: "Category 1 updated",
+      description: "Category 1 description updated",
+      is_active: false,
+    });
+
+    expect(updatedCategory.name).toBe("Category 1 updated");
+    expect(updatedCategory.description).toBe("Category 1 description updated");
+    expect(updatedCategory.is_active).toBeFalsy();
+  });
+});
