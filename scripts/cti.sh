39289efae859feb897b63d586a5a3bbf22301130
Rafael Borges <rafael.borges@luizalabs.com>
2022-08-04 21:27:31 -0300

    update cti command

--- scripts/cti.sh
+++ scripts/cti.sh
@@ -6,4 +6,4 @@ npm run cti create './src/@seedwork/infra'        -- -i '*spec.ts' -b &&
 
 npm run cti create './src/category/application'   -- -i '*spec.ts' -b &&
 npm run cti create './src/category/domain'        -- -i '*spec.ts' -b &&
-npm run cti create './src/category/infra'         -- -i '*spec.ts' -b &&
+npm run cti create './src/category/infra'         -- -i '*spec.ts' -b 
