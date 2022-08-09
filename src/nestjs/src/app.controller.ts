cfd2d894f024b7749a594eee58b729b7868f71e4
Rafael Borges <rafael.borges@luizalabs.com>
2022-08-09 01:46:03 +0000

    wip: config monorepo exports

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// import Category from 'micro-videos-typescript-core/category/domain';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // const category = new Category({ name: 'test' });
+    // console.log('Category: ', Category);
+
     return this.appService.getHello();
   }
 }
