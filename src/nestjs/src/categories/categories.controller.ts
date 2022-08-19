9419b26bdbf2c1b5fea6a31f53c616b26e361cc2
Rafael Borges <rafaelbertelli89@gmail.com>
2022-08-19 19:57:13 -0300

    configuring use of core at nestjs

--- src/nestjs/src/categories/categories.controller.ts
+++ src/nestjs/src/categories/categories.controller.ts
@@ -7,21 +7,31 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+} from 'mvt-core/category/application';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesService: CategoriesService,
+    private readonly createUseCase: CreateCategoryUseCase,
+    private readonly listCategoriesUseCase: ListCategoriesUseCase,
+  ) {}
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.createUseCase.execute({ name: 'AEEE CARAI' });
     return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
   findAll() {
+    return this.listCategoriesUseCase.execute({});
     return this.categoriesService.findAll();
   }
 
--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
+import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+} from 'mvt-core/category/application';
+import { CategoryRepository } from 'mvt-core/category/domain';
+import { CategoryInMemoryRepository } from 'mvt-core/category/infra';
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [
+    CategoriesService,
+    {
+      provide: 'CategoryInMemoryRepository',
+      useClass: CategoryInMemoryRepository,
+    },
+    {
+      provide: CreateCategoryUseCase,
+      useFactory: (repository: CategoryRepository.Repository) => {
+        return new CreateCategoryUseCase(repository);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+    {
+      provide: ListCategoriesUseCase,
+      useFactory: (repository: CategoryRepository.Repository) => {
+        return new ListCategoriesUseCase(repository);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+  ],
 })
 export class CategoriesModule {}
