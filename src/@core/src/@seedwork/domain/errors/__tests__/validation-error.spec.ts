eb62de76c090a3ef24595c07391eb008cace60a7
Rafael Borges <rafaelbertelli89@gmail.com>
2022-08-27 11:38:45 -0300

    fix validation erro

--- src/@core/src/@seedwork/domain/errors/__tests__/validation-error.spec.ts
+++ src/@core/src/@seedwork/domain/errors/__tests__/validation-error.spec.ts
@@ -2,7 +2,7 @@ import { ValidationError } from "../validation-error";
 
 describe("ValidationError", () => {
   it("should assert error", () => {
-    const error = new ValidationError({ id: ["errado"] });
+    const error = new ValidationError("Validation Error");
     expect(error).toBeInstanceOf(Error);
     expect(error).toBeInstanceOf(ValidationError);
     expect(error.name).toBe("ValidationError");
--- src/@core/src/@seedwork/domain/errors/validation-error.ts
+++ src/@core/src/@seedwork/domain/errors/validation-error.ts
@@ -1,8 +1,8 @@
 import { FieldsErrors } from "../validators/validator-fields-interface";
 
 export class ValidationError extends Error {
-  constructor(public error: FieldsErrors) {
-    super("Validation Error");
+  constructor(public message: string) {
+    super(message);
     this.name = "ValidationError";
   }
 }
