38805aed77ecbc4cd3fcf952373d1fb256faa6f6
Rafael Borges <rafaelbertelli89@gmail.com>
2022-08-29 19:43:25 -0300

    implements repository

--- src/@core/src/category/infra/db/sequelize/category.repository.ts
+++ src/@core/src/category/infra/db/sequelize/category.repository.ts
@@ -1,5 +1,6 @@
 import { Category, CategoryRepository } from "#category/domain";
 import { NotFoundError, UniqueEntityId } from "#seedwork/domain";
+import { Op } from "sequelize";
 import { CategoryModel } from "./category.model";
 import { CategoryModelMapper } from "./mappers/category.model.mapper";
 
@@ -16,19 +17,58 @@ export class CategorySequelizeRepository
 
   async findById(id: string | UniqueEntityId): Promise<Category> {
     const model = await this._getBy(`${id}`);
-    const result = CategoryModelMapper.toEntity(model);
-    return result;
+    return CategoryModelMapper.toEntity(model);
   }
 
-  async findAll(): Promise<Category[]> {}
+  async findAll(): Promise<Category[]> {
+    const models = await this.categoryModel.findAll();
+    return models.map(CategoryModelMapper.toEntity);
+  }
 
-  async update(entity: Category): Promise<void> {}
+  async update(entity: Category): Promise<void> {
+    await this._getBy(entity.id);
+    await this.categoryModel.update(entity.toJSON(), {
+      where: { id: entity.id },
+    });
+  }
 
-  async delete(id: string | UniqueEntityId): Promise<void> {}
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id = `${id}`;
+    await this._getBy(_id);
+    this.categoryModel.destroy({ where: { id: _id } });
+  }
 
   async search(
-    query: CategoryRepository.SearchParams
-  ): Promise<CategoryRepository.SearchResult> {}
+    props: CategoryRepository.SearchParams
+  ): Promise<CategoryRepository.SearchResult> {
+    const offset = (props.page - 1) * props.per_page;
+    const limit = props.per_page;
+
+    const { rows: models, count } = await this.categoryModel.findAndCountAll({
+      ...(props.filter && {
+        where: {
+          name: {
+            [Op.like]: `%${props.filter}%`,
+          },
+        },
+      }),
+      ...(props.sort && this.sortableFields.includes(props.sort)
+        ? { order: [[props.sort, props.sort_dir]] }
+        : { order: [["created_at", "DESC"]] }),
+      offset,
+      limit,
+    });
+
+    return new CategoryRepository.SearchResult({
+      items: models.map((m) => CategoryModelMapper.toEntity(m)),
+      current_page: props.page,
+      per_page: props.per_page,
+      total: count,
+      filter: props.filter,
+      sort: props.sort,
+      sort_dir: props.sort_dir,
+    });
+  }
 
   private async _getBy(id: string): Promise<CategoryModel> {
     return await this.categoryModel.findByPk(`${id}`, {
