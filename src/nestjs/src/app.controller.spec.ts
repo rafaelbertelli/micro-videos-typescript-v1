466b908a378e63825efd0a32614a53d392e9bfdc
Rafael Borges <rafaelbertelli89@gmail.com>
2022-09-06 17:35:20 -0300

    config project to use .env

--- src/nestjs/src/app.controller.spec.ts
+++ src/nestjs/src/app.controller.spec.ts
@@ -1,20 +1,40 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 
 describe('AppController', () => {
   let appController: AppController;
+  let app: TestingModule;
+  let configService: ConfigService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: join(__dirname, './envs/.env.test'),
+        }),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    configService = app.get(ConfigService);
+
+    // console.log(app.get(ConfigService).get('DB_VENDOR'));
   });
 
   describe('root', () => {
+    it('should be able to get environment variables', () => {
+      expect(configService.get('DB_VENDOR')).toBeDefined();
+      expect(configService.get('DB_HOST')).toBeDefined();
+      expect(configService.get('DB_LOGGING')).toBeDefined();
+      expect(configService.get('DB_AUTO_LOAD_MODELS')).toBeDefined();
+    });
+
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
